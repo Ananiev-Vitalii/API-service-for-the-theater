12f46670964dac80ebd7899fe3045eacceef6085
Vitalii <ananievvitalii10@gmail.com>
2025-09-04 14:47:37 -0700

    Create theater/messages.py to centralize reusable message constants for import in views and models

---
+++ theater/messages.py
@@ -0,0 +1,5 @@
+class MSG:
+    SUCCESS = "Reservation successful! We will contact you by email shortly."
+    SEAT_TAKEN = (
+        "This seat has just been reserved by someone else. Please choose another."
+    )
