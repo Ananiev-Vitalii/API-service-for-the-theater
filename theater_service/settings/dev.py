8890874a2d72c9f1be0b9566fcfd05494608b160
Vitalii <ananievvitalii10@gmail.com>
2025-10-03 11:22:04 -0700

    chore(settings/deploy): refine dev/prod configs, add WhiteNoise, and production start script

--- theater_service/settings/dev.py
+++ theater_service/settings/dev.py
@@ -1,11 +1,10 @@
-from .base import *  # noqa
+from theater_service.settings.base import *  # noqa
 import os
 
 
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-
 
 DATABASES = {
     "default": {
@@ -17,11 +16,3 @@ DATABASES = {
         "PORT": int(os.getenv("POSTGRES_PORT", "5432")),
     }
 }
-
-
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
-CELERY_TASK_IGNORE_RESULT = False
-CELERY_TASK_TRACK_STARTED = True
-CELERY_TIMEZONE = "America/Vancouver"
-CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
--- theater_service/settings/prod.py
+++ theater_service/settings/prod.py
@@ -1,30 +1,18 @@
-# from .base import *
-#
-# DEBUG = False
-#
-# ALLOWED_HOSTS = ["127.0.0.1"]
-#
-# RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
-# if RENDER_EXTERNAL_HOSTNAME:
-#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-#
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.environ["POSTGRES_DB"],
-#         "USER": os.environ["POSTGRES_USER"],
-#         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-#         "HOST": os.environ["POSTGRES_HOST"],
-#         "PORT": int(os.environ["POSTGRES_DB_PORT"]),
-#
-#     }
-# }
-#
-# # AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
-# # AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
-# # AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
-# # AWS_S3_REGION_NAME = os.environ["AWS_S3_REGION_NAME"]
-# # AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-# MEDIA_ROOT = ""
-# # MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+from theater_service.settings.base import *  # noqa
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": int(os.environ.get("POSTGRES_DB_PORT", 5432)),
+        "OPTIONS": {"sslmode": "require"},
+    }
+}
